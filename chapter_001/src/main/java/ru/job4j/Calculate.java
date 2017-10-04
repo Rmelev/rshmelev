e1c2e018f2baf53c8ed381929a30c03ec1964f10
Rmelev <roman.shmelev@gmail.com>
2017-10-04 18:05:24 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
 
+/**
+  * Класс Calculate является первым показательным классом для демонстрации работы Java.
+  * Содержит в себе единственный метод main
+*/
 public class Calculate {
+
+  /**
+    * Выводит сообщение "Hello, World!"
+    * @param Принимает в массив элементов типа String
+    * @return Ничего не возвращает, ибо void
+  */
   public static void main(String[] args) {
 	System.out.println("Hello, World!");
   }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,9 @@
+
+/**
+* Package for calculate task.
+*
+* @author Roman Shmelev (mailto:roman.shmelev@gmail.com)
+* @version Code have formatted by rules
+* @since version 0.1
+*/
+package ru.job4j;
