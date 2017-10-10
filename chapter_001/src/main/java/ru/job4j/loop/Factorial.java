67eecbe397a11f81a81fa910c9e60f7546477b64
Rmelev <roman.shmelev@gmail.com>
2017-10-10 13:46:29 +0300

    Task #193

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+class Factorial {
+    /**
+     * Calculate factorial for n.
+     * @param n - value for factorial calculate
+     * @return - fanal value of n!.
+     */
+    public int calc(int n) {
+        if (n==0) return 1;
+        int mult = 1;
+        for (int i = 1; i <= n; i++) {
+            mult *= i;
+        }
+        return mult;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FactorialTest {
+    /**
+     * Factorial if n = 5.
+     */
+    @Test
+    public void whenN5ThenFactorial120() {
+        Factorial fact = new Factorial();
+        int result = fact.calc(5);
+        assertThat(result, is(120));
+    }
+
+    /**
+     * Factorial if n = 5.
+     */
+    @Test
+    public void whenN0ThenFactorial1() {
+        Factorial fact = new Factorial();
+        int result = fact.calc(0);
+        assertThat(result, is(1));
+    }
+}
