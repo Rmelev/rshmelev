6db071cc744d0db26769b213cae41d9394d36c5f
Rmelev <roman.shmelev@gmail.com>
2017-11-19 16:58:17 +0300

    Task #999.

---
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,21 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * user info.
+ */
+public class User {
+    /**
+     * user's name.
+     */
+    String name;
+    /**
+     * number of children.
+     */
+    int children;
+    /**
+     * date of birthday.
+     */
+    Calendar birthday;
+}
