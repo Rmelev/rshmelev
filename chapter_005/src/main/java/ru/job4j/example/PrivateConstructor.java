a82f2e03e24fb3801e3dad582d6862e7f7af28eb
Rmelev <roman.shmelev@gmail.com>
2017-12-25 12:34:17 +0300

    Technical commit.

--- chapter_005/src/main/java/ru/job4j/example/PrivateConstructor.java
+++ chapter_005/src/main/java/ru/job4j/example/PrivateConstructor.java
@@ -46,3 +46,70 @@ public class PrivateConstructor {
     }
 
 }*/
+
+/*package ru.job4j.example;
+public class PrivateConstructor {
+    int n;
+    boolean valueSet = false;
+    synchronized int get() {
+        while (!valueSet)
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println("Искл перехвачено");
+            }
+        System.out.println("Получено: " + n);
+        valueSet = false;
+        notify();
+        return n;
+    }
+    synchronized void put(int n) {
+        while (valueSet)
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println("Искл перехвачено");
+            }
+        this.n = n;
+        valueSet = true;
+        System.out.println("Отправлено: " + n);
+        notify();
+    }
+}
+
+class Producer implements Runnable {
+    PrivateConstructor q;
+    Producer(PrivateConstructor q) {
+        this.q = q;
+        new Thread(this, "Поставщик").start();
+    }
+    public void run() {
+        int i = 0;
+        while(true) {
+            q.put(i++);
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    PrivateConstructor q;
+    Consumer(PrivateConstructor q) {
+        this.q = q;
+        new Thread(this, "Потребитель").start();
+    }
+    public void run() {
+        while(true) {
+            q.get();
+        }
+    }
+}
+
+class PCFixed {
+    public static void main(String[] args) {
+        PrivateConstructor q = new PrivateConstructor();
+        new Producer(q);
+        new Consumer(q);
+        System.out.println("Нажмите Сtrl+C");
+    }
+}
+*/
