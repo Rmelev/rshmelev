84df050f1d913e6fac46941fbd0c434b63b09d57
Rmelev <roman.shmelev@gmail.com>
2018-02-04 15:51:04 +0300

    Technical commit.

---
+++ chapter_008/src/main/java/ru/job4j/testtask/Vacancy.java
@@ -0,0 +1,41 @@
+/*package ru.job4j.testtask;
+
+import java.sql.Timestamp;
+
+public class Vacancy {
+    String description;
+    String url;
+    Timestamp date;
+
+    public Vacancy(String description, String url, Timestamp date) {
+        this.description = description;
+        this.url = url;
+        this.date = date;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+}
+*/
