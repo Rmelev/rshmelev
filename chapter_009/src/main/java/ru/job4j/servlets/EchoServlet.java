de17155ec9135426ee8bff822ec4d92e532c025b
Rmelev <roman.shmelev@gmail.com>
2018-02-08 14:30:13 +0300

    Technical commit.

--- chapter_009/src/main/java/ru/job4j/servlets/EchoServlet.java
+++ chapter_009/src/main/java/ru/job4j/servlets/EchoServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * First servlet.
@@ -18,6 +20,10 @@ public class EchoServlet extends HttpServlet {
      * Logger.
      */
     public static final Logger LOG = LoggerFactory.getLogger(EchoServlet.class);
+    /**
+     * list of users.
+     */
+    private List<String> users = new CopyOnWriteArrayList<>();
 
     /**
      * doGet.
@@ -30,8 +36,22 @@ public class EchoServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.append("Hello, World!");
+        writer.append("Hello, World!" + this.users);
         writer.flush();
     }
 
+    /**
+     * doPost.
+     * @param req - request.
+     * @param resp - response.
+     * @throws ServletException - exp.
+     * @throws IOException - exp.
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        String login = req.getParameter("login");
+        users.add(login);
+        doGet(req, resp);
+    }
 }
